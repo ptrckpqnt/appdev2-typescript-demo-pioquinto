c54b385aaf2477534f15b31f805b55bf7a7ee119
ptrckpqnt <patrickpioquinto2325.com>
2026-03-27 15:59:57 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,6 @@
-let userName1 = "Elmer";
-
+let userName1 = "Patrick";
+let age = 22
+let isGoingToPass = true
 console.log(typeof userName1)
+console.log(typeof age)
+console.log(typeof isGoingToPass)
