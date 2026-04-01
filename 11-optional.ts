caab02ce8bff98a273fd03dbc15f2685463a85e3
ptrckpqnt <patrickpioquinto2325.com>
2026-04-01 15:51:59 +0800

    Done playing and updating the 11-optional.ts file

--- 11-optional.ts
+++ 11-optional.ts
@@ -1,14 +1,11 @@
-// Optional Values in parameter
 function generateError(msg?: string) {
-    throw new Error(msg)
+    console.log("Message:", msg);
 }
 
-generateError("An error occured") // Automatic Semicolon Insertion pitfall - add ; to explicitly tell that this is a function
-// generateError()
-
+generateError("An error occured");
+generateError();
 
 (() => {
-    // Optional Values in object properties
     type User = {
         name: string;
         age: number;
@@ -16,22 +13,16 @@ generateError("An error occured") // Automatic Semicolon Insertion pitfall - add
     }
 
     let user: User = {
-        name: 'Elmer',
-        age: 31
+        name: 'Patrick',
+        age: 22
     }
 
-    user.name
-    user.age
-})
+    console.log("User:", user);
+})();
 
-// Nullish Coalescing `??` operator 
 let input = ''
 const didProvideInput = input ?? false;
+console.log("Using ?? :", didProvideInput);
 
-// try it on browser console log to see results
-// update it `??` to `||`
-// `||` check for falsy not false - falsy = null, undefined, 0, empty string, etc
-// ?? check only if null or undefined
-
-// Use || when you want to fall back on any falsy value.
-// Use ?? when you only want to fall back on null or undefined.
+const usingOr = input || false;
+console.log("Using || :", usingOr);
