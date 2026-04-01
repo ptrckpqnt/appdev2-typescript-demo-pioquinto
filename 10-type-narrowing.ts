861ad410a9d72418bcf620f9932c35e36cf62b72
ptrckpqnt <patrickpioquinto2325.com>
2026-04-01 15:48:57 +0800

    Done playing and updating the 10-form.html file & 10-type-narrowing.ts file

--- 10-type-narrowing.ts
+++ 10-type-narrowing.ts
@@ -1,23 +1,9 @@
 const inputEl = document.getElementById('user-name')
 
-console.log(inputEl.value) // This line is unsafe because inputEl might be null
+console.log("Raw element:", inputEl);
 
 if (!inputEl) {
     throw new Error('Element not found!');
 }
 
-console.log(inputEl.value)
-
-// You can convince TypeScript that you are sure the value will not be null by using the non-null assertion operator `!`
-// You can use the optional chaining `?.` operator to safely check if value is not null before the next operation
-
-// const inputEl = document.getElementById('user-name')!
-// console.log(inputEl!.value)
-// if (!inputEl) {
-//     throw new Error('Element not found!');
-// }
-// console.log(inputEl?.value)
-
-
-// Type Casting or Assertion
-// const inputEl = document.getElementById('user-name') as HTMLInputElement | null
+console.log("Safe value:", (inputEl as HTMLInputElement).value);
